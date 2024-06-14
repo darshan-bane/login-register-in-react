d5672cdf9595e1be7d86c8fee721e1ac1e71c421
darshan-bane <darshanbane253@gmail.com>
2024-06-14 13:19:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
+import { faEnvelope, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 function App() {
+
+  let [action, setAction] = useState("Sign Up")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='header'>
+          <h1>{action}</h1>
+          <div className='underline'></div>
+        </div>
+        <div className='inputs'>
+          {action === "Log In" ? <div></div> : <div className='input'>
+            <FontAwesomeIcon icon={faUser} />
+            <input type='text' name='uname' placeholder='Name'></input>
+          </div>}
+
+          <div className='input'>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <input type='email' name='eamil' placeholder={action === "Log In" ? 'Enter your Email Id' : 'Email Id'}></input>
+          </div>
+          <div className='input'>
+            <FontAwesomeIcon icon={faLock} />
+            <input type='password' name='password' placeholder={action === "Log In" ? 'Enter your Password' : 'Password'}></input>
+          </div>
+        </div>
+        {action === "Sign Up" ? <div></div> : <div className='forget_password'>Lost Pasword? <a href=''>Click Here</a></div>}
+
+        <div className='submit_container'>
+          <button className={action === "Log In" ? 'submit gray' : 'submit'} onClick={() => { setAction('Sign Up') }}>Sign Up</button>
+          <button className={action === "Sign Up" ? 'submit gray' : 'submit'} onClick={() => { setAction('Log In') }}>Log In</button>
+        </div>
+      </div>
+    </>
   );
 }
 
